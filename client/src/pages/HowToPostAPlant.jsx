04bd152962a4f6c247eb0181d6cf2842fbdc540a
Elinor Chayo <elliechayo@gmail.com>
2023-07-12 19:53:33 +0530

    changed heading layout

--- client/src/pages/HowToPostAPlant.jsx
+++ client/src/pages/HowToPostAPlant.jsx
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 
 // assets
 import HeroImage from "../assets/postaplant.jpeg";
+import { Box } from "@chakra-ui/react";
 
 export default function HowToPostAPlant() {
   return (
     <MainWrapper>
-      <img src={HeroImage} alt="" />
-      <h2 className="title">How To Post A Plant</h2>
+      <HeadingWrapper imagepath={HeroImage}>
+        <Box as="div" className="titleWrapper">
+          <h2 className="title">How To Post A Plant</h2>
+        </Box>
+      </HeadingWrapper>
+      {/* <img src={HeroImage} alt="" /> */}
       <div className="content">
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
@@ -63,5 +68,29 @@ const MainWrapper = styled.main`
     background: var(--green-medium);
     color: white;
     display: inline-block;
+  }
+`;
+
+const HeadingWrapper = styled.section`
+  min-height: 400px;
+  height: 100%;
+  background: url(${(props) => props.imagepath}),
+    linear-gradient(to right, #dddcd8, #efeeea);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+
+  .titleWrapper {
+    padding-top: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    max-width: 600px;
+    margin: 0 auto;
+    height: 100%;
+
+    h2 {
+      font-size: clamp(38px, 5vw, 48px);
+    }
   }
 `;
