18e583693cbcd621f6925831849ba528bcbd5370
Elinor Chayo <elliechayo@gmail.com>
2023-07-12 06:04:42 -0400

    Update config.js with heruko url

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-export const backend_URL = "http://localhost:5000";
+export const backend_URL =  "https://greenhouse-chayo-92a31028ba3b.herokuapp.com/";
 export const homepage_URL = "http://localhost:3000";
 export const stripeKey = "";
 export const priceId = "";
