738e7487fde3d81291666dc70b24d55fa04fc0cc
Elinor Chayo <elliechayo@gmail.com>
2023-07-12 05:30:05 -0400

    replace , ,  in the server folder and  in the client/components/Account folder and added Procfile to root to enable heruko deployment and connection to atlasDB

--- server/multer.js
+++ server/multer.js
@@ -1,7 +1,9 @@
 const multer = require("multer");
+const path = require("path");
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/uploads");
+    cb(null, path.resolve(__dirname, "./public/uploads"));
   },
   filename: function (req, file, cb) {
     const uniqueName = new Date().getTime() + "-" + file.originalname;
@@ -11,3 +13,4 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 module.exports = upload;
+
--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const { ApolloServer } = require("apollo-server-express");
 
 const typeDefs = require("./schema/types");
 const resolvers = require("./schema/resolver");
-
 
 const connectDB = require("./config/db");
 const upload = require("./multer");
@@ -27,14 +27,21 @@ async function startServer() {
   // apply the apollo server middleware to the app
   const app = express();
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(__dirname + "/public"));
   app.use(cors());
+
+  app.use(express.static(path.resolve(__dirname, "./public")));
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+
   app.post("/addMedia", upload.single("uploadedFile"), (req, res) => {
     if (!req.file) {
       return res.json({ success: false });
     } else {
       return res.json({ success: true, file: req.file.filename });
     }
+  });
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 
   server.applyMiddleware({ app });
