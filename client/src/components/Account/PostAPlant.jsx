d46f2bdb550b7c9027378657261a695c8eab40e2
Elinor Chayo <elliechayo@gmail.com>
2023-07-12 19:45:47 +0530

    Fixed the Category field

--- client/src/components/Account/PostAPlant.jsx
+++ client/src/components/Account/PostAPlant.jsx
@@ -80,10 +80,10 @@ export default function PostAPlant({ user }) {
             value={formFields.category}
             onChange={handleInputChange}
           >
-            <option value="category-1">Category 1</option>
-            <option value="category-2">Category 2</option>
-            <option value="category-3">Category 3</option>
-            <option value="category-4">Category 4</option>
+            <option value="category-1">Fig Trees</option>
+            <option value="category-2">Maple Trees</option>
+            <option value="category-3">Shrub Tress</option>
+            <option value="category-4">Fruit Trees</option>
           </Select>
         </div>
         <div className="form-group">
