7ec320c74fb6a5077aa21a6f1055f30d1fcc43bb
Elinor Chayo <elliechayo@gmail.com>
2023-07-13 00:52:32 -0400

    removed serviceWorker and '/' from backendURL

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-export const backend_URL =  "https://greenhouse-team-c08c7033be08.herokuapp.com/";
+export const backend_URL =  "https://greenhouse-team-c08c7033be08.herokuapp.com";
 export const homepage_URL = "http://localhost:3000";
 export const stripeKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
 export const priceId = "";
